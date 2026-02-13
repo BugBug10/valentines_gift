c7e50cce10f2d86917ffeb62abb9e565512fcfde
BugBug10 <isabellawells2003@gmail.com>
2026-02-13 01:49:49 -0500

    changed name of heart icon

--- yes.js
+++ yes.js
@@ -2,7 +2,7 @@
 function kiss_rain() {
   const kiss = document.createElement("img");
 
-  kiss.src = "imgs/Soft-Pink-Heart-Symbol-PNG.PNG"
+  kiss.src = "imgs/heart.png"
   kiss.classList.add("falling-kiss");
 
   kiss.style.left = Math.random() * 100 + "vw";
@@ -24,5 +24,10 @@ function kiss_rain() {
 window.onload = () => {
   for (let i = 0; i < 40; i++) {
     setTimeout(kiss_rain, i * 100);
+  }
+  if (no_count == 3) {
+    let remark = document.createElement("p");
+    remark.innerText = "Too bad! You will be my valentine.";
+    document.appendChild(remark);
   }
 };
